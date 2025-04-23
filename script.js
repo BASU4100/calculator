50678bcce5cd626f2c252960973eb2fe85e6b48e
Vivek Kumar <vk13114416@gmail.com>
2025-04-23 18:28:19 +0000

    Gave the logic for the calculator.

---
+++ script.js
@@ -0,0 +1,112 @@
+const operatorSide = document.querySelector("#operatorSide");
+const selectedOperator = document.querySelector("#selectedOperator");
+const values = document.querySelector("#values");
+const operandSide = document.querySelector("#operandSide");
+let value = values.textContent;
+let isDot = false;
+let isOperatorSelected = false;
+let isValueTaken = false;
+let isEvaluated = false;
+let firstOperand = "";
+let secondOperand = "";
+let operator = "";
+
+operatorSide.addEventListener("click", (e) => {
+    if (isOperatorSelected && isValueTaken) {
+        value = evaluate();
+        isEvaluated = true;
+        display();
+    }
+    isOperatorSelected = true;
+    isValueTaken = false;
+    let op = e.target.id[1];
+    let ops = selectedOperator.children;
+    for (let key in ops) {
+        let id = ops[key].id;
+        if (id != null) {
+            const temp = document.querySelector("#o\\"+id[1]);
+            if (id[1] === op)
+                addRemoveStyle(temp, true);
+            else
+                addRemoveStyle(temp, false);
+        }
+    }
+    if (isOperatorSelected && !isValueTaken) {
+        operator = op;
+        firstOperand = value;
+    }
+});
+
+function addRemoveStyle(elem, change) {
+    if (change)
+        elem.setAttribute("style", "color: black;");
+    else
+        elem.setAttribute("style", "");
+}
+
+operandSide.addEventListener("click", (e) => {
+    let id = e.target.id;
+    if (id == "AC") {
+        isOperatorSelected = false;
+        isValueTaken = false;
+        isDot = false;
+        let ops = selectedOperator.children;
+        for (let key in ops) {
+            if (ops[key].id != null)
+                addRemoveStyle(ops[key], false);
+        }
+        value = 0;
+    }
+    else if (id == "dot") {
+        value += ((!isDot)?".":"");
+        isDot = true;
+        isValueTaken = true;
+    }
+    else if (id == "k=") {
+        value = evaluate();
+        isEvaluated = true;
+        isOperatorSelected = false;
+        isValueTaken = false;
+        let ops = selectedOperator.children;
+        for (let key in ops) {
+            if (ops[key].id != null)
+                addRemoveStyle(ops[key], false);
+        }
+    }
+    else {
+        value = ((value==="0" || !isValueTaken)?"":value);
+        if (value.length==6) {
+            alert("Only 6 digits can be taken.");
+            return;
+        }
+        value += id[1];
+        isValueTaken = true;
+        if (isOperatorSelected)
+            secondOperand = value;
+    }
+    display();
+});
+
+function evaluate() {
+    if (!isOperatorSelected || (isOperatorSelected && !isValueTaken))
+        return value;
+    else {
+        switch (operator) {
+            case "*" : return firstOperand * secondOperand;
+            case "/" : return ((secondOperand == 0)?"IDK": firstOperand / secondOperand);
+            case "-" : return firstOperand - secondOperand;
+            default : return +firstOperand + +secondOperand;
+        }
+    }
+}
+
+function display() { 
+    if (isEvaluated && value !== "IDK") {
+        let arr = (value+"").split(".");
+        values.textContent = (+value).toFixed((arr[1] > 0)?Math.min(6-(arr[0].length+1), arr[1].length):0);
+        isEvaluated = false;
+    }
+    else {
+        values.textContent = value;
+    }
+}
